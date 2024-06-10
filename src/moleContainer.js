e9cef70199eee41c2784d8f05590b93fba05a3eb
leo-mp3 <leonardohigareda@Diet-Mountain-Dew.local>
2024-06-10 09:55:52 -0700

    added new functions

---
+++ src/moleContainer.js
@@ -0,0 +1,12 @@
+import Mole from "./mole";
+
+function MoleContainer(props){
+    return(
+        <div>
+            <h2>MoleContainer</h2>
+            <Mole></Mole>
+        </div>
+    )
+
+}
+export default MoleContainer
